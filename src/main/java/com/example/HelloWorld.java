c426eb157b82c42badbd1f29c01b4e4094add9d1
xuen <tiuhanxuen@gmail.com>
2026-04-06 15:49:34 +0800

    second commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //comment
+        System.out.println("Hello, students!"); //comment
     }
 
     public String getGreeting() {
